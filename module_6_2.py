e1dd776e104abeb707570ed8fa1f8d75de18c726
Juvikk <alex.kondor2002@mail.ru>
2024-09-02 19:04:48 +0300

    Update module_6_2.py

--- module_6_2.py
+++ module_6_2.py
@@ -1,39 +1,48 @@
 class Vehicle:
-    def __init__(self, owner: str, model: str, color='purple', engine_power=3500):
+    __COLOR_VARIANTS = ['black,purple,white,pink']
+
+    def __init__(self, owner, model, engine_power, color):
         self.owner = owner
-        self.model = model
-        self.engine_power = engine_power
-        self.color = color
+        self.__model = model
+        self.__engine_power = engine_power
+        self.__color = color
 
-        def get_model(self):
-            return f"Модель: {self.__model}"
+    def get_model(self):
+        return f"Модель: {self.__model}"
 
-        def get_horsepower(self):
-            return f"Мощность двигателя: {self.__engine_power}"
+    def get_horsepower(self):
+        return f"Мощность двигателя: {self.__engine_power}"
 
     def get_color(self):
         return f"Цвет: {self.__color}"
 
     def print_info(self):
-        print(self.get_model(), self.get_horsepower(), self.get_color(),
-              f'Владелец: {self.owner}')
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
+        print(f"Владелец: {self.owner}")
 
-        def set_color(self, new_color: str):
-            if new_color.lower() in self.__COLOR_VARIANTS:
-                self.__color = new_color
-            else:
-                print('91mНельзя сменить цвет на {new_color}')
-
+    def set_color(self, new_color):
+        if new_color.lower() in map(str.lower, self.__COLOR_VARIANTS):
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {new_color}")
 
 
 class Sedan(Vehicle):
-    __PASSENGERS_LIMIT = 5
+    __PASSENGERS_LIMIT = 4
+
+    def __init__(self, owner, model, color, engine_power):
+        super().__init__(owner, model, engine_power, color)
+
 
 vehicle = Sedan('dadya fyodor', 'zhigyli', 'purple', 3500)
 
-
 vehicle.print_info()
 
-vehicle.set_color('white')
-vehicle.engine_power('1500')
+vehicle.set_color ='white'
+vehicle.engine_power = 1500
 vehicle.owner = 'dadya petya'
+vehicle.print_info()
+
+
